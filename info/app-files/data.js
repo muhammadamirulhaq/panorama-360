8032027795def4db1df97145e740045b2645624d
Muhammad Amirul Haq <70356768+muhammadamirulhaq@users.noreply.github.com>
2023-07-20 15:30:00 +0800

    Add files via upload

---
+++ info/app-files/data.js
@@ -0,0 +1,45 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-rektorat",
+      "name": "Rektorat",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -1.7211514611430054,
+        "pitch": -0.033499856627198454,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.021812008513414582,
+          "pitch": -0.254189965932035,
+          "title": "Gedung Rektorat",
+          "text": "8 Lantai"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
