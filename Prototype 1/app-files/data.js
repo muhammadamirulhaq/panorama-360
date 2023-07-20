013910e1f8b48e82c72f3e42c1cd8d2024bf9174
Muhammad Amirul Haq <70356768+muhammadamirulhaq@users.noreply.github.com>
2023-07-20 20:59:16 +0800

    Add files via upload

---
+++ Prototype 1/app-files/data.js
@@ -0,0 +1,45 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-rektorat",
+      "name": "Rektorat",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.010642528026062692,
+        "pitch": -0.3825151812296159,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.0024814207016135015,
+          "pitch": -0.14462529503537702,
+          "title": "Gedung Rektorat",
+          "text": "<div><span style='color:#dfdfdf;'>Go to</span> <a href='http://alexbartl.de/photos_elementares.html'>Lantai 1</a></div><div><span style='color:#dfdfdf;'>Go to</span> <a href='#'>Lantai 2</a></div><div><span style='color:#dfdfdf;'>Go to</span> <a href='#'>Lantai 3</a></div><div><span style='color:#dfdfdf;'>Go to</span> <a href='#'>Lantai 4</a></div><div><span style='color:#dfdfdf;'>Go to</span> <a href='#'>Lantai 5</a></div><div><span style='color:#dfdfdf;'>Go to</span> <a href='#'>Lantai 6</a></div><div><span style='color:#dfdfdf;'>Go to</span> <a href='#'>Lantai 7</a></div><div><span style='color:#dfdfdf;'>Go to</span> <a href='#'>Lantai 8</a><br></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
